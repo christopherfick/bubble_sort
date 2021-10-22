01d1fb44bf422dfe2afd23e3878e8d404482f212
Christopher Fick <83765940+KOMelite@users.noreply.github.com>
2021-10-22 15:50:08 +0200

    Added unittesting

---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+from bubblesort import main 
+
+class TestMain(unittest.TestCase):
+
+    def test_main(self):
+       self.assertEqual(main.bubble_sort([5,4,3,2,1]), [1,2,3,4,5])
+
